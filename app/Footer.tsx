1e373d8914de07036f51fab62bc8595b71d6ed7f
Ahsan Khan <ahsan@geeksofkolachi.com>
2023-07-31 11:42:56 +0500

    chore: add daisyui and update configs

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,7 +1,5 @@
 const Footer: React.FC = () => (
-  <div className='mx-auto my-10 mb-5 text-xs'>
-    Copyright © {new Date().getFullYear()}
-  </div>
+  <div className='mx-auto my-10 mb-5 text-xs'>Made with ♥</div>
 );
 
 export default Footer;
