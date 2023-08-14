892cf7bbfdc7cd3d43cd125cf8f138b3a5c12440
Ahsan Khan <m.ahsankhan26@hotmail.com>
2023-08-15 01:59:06 +0500

    CatGPT text input (#5)
    * feat: cat-gpt ready
    
    * feat: focus after prompt end

--- app/prompt/page.tsx
+++ app/prompt/page.tsx
@@ -1,61 +1,115 @@
 'use client';
 
 import { useState } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { AiOutlineSend } from 'react-icons/ai';
 import {
   generateMessage,
   getRandomBetweenRange,
   LETTER_RANGE,
 } from 'app/prompt/utils';
 
-import { Button, Hero } from '@/components';
+import { Hero } from '@/components';
+import Input from '@/components/Input';
+
+interface IForm {
+  message: string;
+}
 
 const Prompt: React.FC = () => {
+  const { control, handleSubmit, reset } = useForm<IForm>({
+    defaultValues: {
+      message: '',
+    },
+  });
+
   const [messages, setMessages] = useState<string[]>([]);
   const [visibleText, setVisibleText] = useState('');
 
-  const showPrompt = () => {
-    const message = generateMessage();
-    const interval = setInterval(() => {
-      setVisibleText((prev) => {
-        // check if prompt over
-        if (prev.length >= message.length) {
-          clearInterval(interval);
-          setMessages([...messages, prev]);
-          return '';
-        }
-        // set new value continuing prev value length
-        const textToSet = `${prev}${message.slice(
-          prev.length,
-          prev.length + getRandomBetweenRange(LETTER_RANGE),
-        )}`;
-        return textToSet;
-      });
-    }, 100);
+  const showPrompt = ({ messagesArr }: { messagesArr: string[] }) => {
+    return new Promise<void>((resolve) => {
+      const message = generateMessage();
+      const interval = setInterval(() => {
+        setVisibleText((prev) => {
+          // check if prompt over
+          if (prev.length >= message.length) {
+            clearInterval(interval);
+            resolve();
+            setMessages([...messagesArr, prev]);
+            return '';
+          }
+          // set new value continuing prev value length
+          const textToSet = `${prev}${message.slice(
+            prev.length,
+            prev.length + getRandomBetweenRange(LETTER_RANGE),
+          )}`;
+          return textToSet;
+        });
+      }, 100);
+    });
+  };
+
+  const newPrompt: SubmitHandler<IForm> = async ({ message }) => {
+    const updatedMessages = [...messages, message];
+    setMessages(updatedMessages);
+    reset();
+    await showPrompt({ messagesArr: updatedMessages });
+    const input: HTMLInputElement | null = document.querySelector('#message');
+    input?.focus();
   };
 
   return (
     <div>
-      <Hero title='CatGPT' subtitle='Purrfect Conversations!' />
+      <Hero subtitle='Purrfect Conversations!' title='CatGPT' />
       <div className='flex-center flex-col'>
         <div className='my-10 flex h-96 w-full flex-col-reverse justify-between overflow-y-scroll rounded-lg bg-base-200 p-10'>
-          <div className='my-10 flex flex-col gap-4'>
+          <div className='my-10 flex flex-col gap-4 '>
             {messages?.map((message, idx) => (
-              <div key={`${message}_${idx}`}>{message}</div>
+              <div
+                className='even:text-primary-content'
+                key={`${message}_${idx}`}
+              >
+                {message}
+              </div>
             ))}
-            <span className='flex items-center'>
+            <span className='flex items-center text-primary-content'>
               {visibleText}
               <div className='h-full w-0.5 bg-info' /> {/* cursor */}
             </span>
           </div>
         </div>
-        <Button
-          disabled={!!visibleText}
-          loading={!!visibleText}
-          fullWidth
-          onClick={showPrompt}
-        >
-          Generate
-        </Button>
+
+        <form className='contents' onSubmit={handleSubmit(newPrompt)}>
+          <Controller
+            control={control}
+            name='message'
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <Input
+                disabled={!!visibleText}
+                error={error?.message}
+                icon={<AiOutlineSend />}
+                iconDisabled={!!visibleText}
+                id='message'
+                onChange={onChange}
+                placeholder='Send a message'
+                value={value}
+              />
+            )}
+            rules={{
+              required: { value: true, message: 'Message cannot be empty' },
+              validate: {
+                checkEmptySpaces: (value: string) => {
+                  if (typeof value === 'string') {
+                    if (!value?.trim()?.length) {
+                      return 'Message cannot be empty';
+                    }
+                  }
+                  return true;
+                },
+              },
+            }}
+          />
+        </form>
       </div>
     </div>
   );
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -3,3 +3,4 @@
 export const isUndefined = (value: any) => value === undefined;
 export const isDefined = (value: any) => value !== undefined;
 export const isNull = (value: any) => value === null;
+export const isEven = (value: number) => value % 2 === 0;
