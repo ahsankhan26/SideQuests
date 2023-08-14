ea23c489251890cb170f28f795df67a12eb54f0c
Ahsan Khan <m.ahsankhan26@hotmail.com>
2023-08-14 15:53:41 +0500

    Grid Visualisation (#4)
    * feat: add grid-flex (WIP)
    
    * feat: single configurable class
    
    * feat: grid visualisation done
    
    * feat: grid visualise ready

---
+++ app/grid/(Components)/utils.ts
@@ -0,0 +1,72 @@
+export interface IConfiguration {
+  itemCount: number;
+  columns: number;
+  gap: number;
+}
+
+export interface IAdvanceConfiguration {
+  colSpan?: number | 'auto';
+  colStart?: number | 'auto';
+}
+
+export const getGridCols = (columns: number) => {
+  return {
+    'grid-cols-1': columns === 1,
+    'grid-cols-2': columns === 2,
+    'grid-cols-3': columns === 3,
+    'grid-cols-4': columns === 4,
+    'grid-cols-5': columns === 5,
+    'grid-cols-6': columns === 6,
+    'grid-cols-7': columns === 7,
+    'grid-cols-8': columns === 8,
+    'grid-cols-9': columns === 9,
+    'grid-cols-10': columns === 10,
+    'grid-cols-11': columns === 11,
+    'grid-cols-12': columns === 12,
+  };
+};
+
+export const getGridGap = (gap: number) => {
+  return {
+    'gap-0': gap === 0,
+    'gap-1': gap === 1,
+    'gap-2': gap === 2,
+    'gap-3': gap === 3,
+    'gap-4': gap === 4,
+    'gap-5': gap === 5,
+  };
+};
+
+export const getColSpan = (colSpan: number | 'auto' | undefined) => {
+  return {
+    'col-auto': colSpan === 'auto',
+    'col-span-1': colSpan === 1,
+    'col-span-2': colSpan === 2,
+    'col-span-3': colSpan === 3,
+    'col-span-4': colSpan === 4,
+    'col-span-5': colSpan === 5,
+    'col-span-6': colSpan === 6,
+    'col-span-7': colSpan === 7,
+    'col-span-8': colSpan === 8,
+    'col-span-9': colSpan === 9,
+    'col-span-10': colSpan === 10,
+    'col-span-11': colSpan === 11,
+    'col-span-12': colSpan === 12,
+  };
+};
+export const getColStart = (colStart: number | 'auto' | undefined) => {
+  return {
+    'col-auto': colStart === 'auto',
+    'col-start-2': colStart === 2,
+    'col-start-3': colStart === 3,
+    'col-start-4': colStart === 4,
+    'col-start-5': colStart === 5,
+    'col-start-6': colStart === 6,
+    'col-start-7': colStart === 7,
+    'col-start-8': colStart === 8,
+    'col-start-9': colStart === 9,
+    'col-start-10': colStart === 10,
+    'col-start-11': colStart === 11,
+    'col-start-12': colStart === 12,
+  };
+};
---
+++ app/grid/page.tsx
@@ -0,0 +1,197 @@
+'use client';
+
+import { useMemo, useState } from 'react';
+import { SelectInput } from 'app/grid/(Components)/Common';
+import {
+  getColSpan,
+  getColStart,
+  getGridCols,
+  getGridGap,
+  IAdvanceConfiguration,
+  IConfiguration,
+} from 'app/grid/(Components)/utils';
+import RangeInput from 'app/sort-visualise/(Components)/RangeInput';
+import classNames from 'classnames';
+
+import { Hero } from '@/components';
+
+const GridFlex: React.FC = () => {
+  const [configuration, setConfiguration] = useState<IConfiguration>({
+    itemCount: 6,
+    columns: 3,
+    gap: 2,
+  });
+  const [advanceConfiguration, setAdvanceConfiguration] = useState<
+    IAdvanceConfiguration[]
+  >([]);
+
+  const items = useMemo(
+    () => new Array(configuration.itemCount).fill(0).map((_, idx) => idx + 1),
+    [configuration.itemCount],
+  );
+
+  const handleConfigurationChange = (val: Partial<IConfiguration>) => {
+    const temp = { ...configuration };
+    type ParamKeys = keyof typeof val;
+    (Object.keys(val) as ParamKeys[]).forEach((key) => {
+      const value = val[key];
+      if (value !== undefined) {
+        temp[key] = value;
+      }
+    });
+    setConfiguration(temp);
+  };
+
+  const handleAdvanceConfigurationChange = (
+    val: Partial<IAdvanceConfiguration>,
+    index: number,
+  ) => {
+    const duplicateArr = [...advanceConfiguration];
+    const temp = advanceConfiguration[index - 1] ?? {};
+
+    type ParamKeys = keyof typeof val;
+    (Object.keys(val) as ParamKeys[]).forEach((key) => {
+      const value = val[key];
+      if (value !== undefined) {
+        temp[key] = value;
+      }
+    });
+    if (temp) {
+      duplicateArr[index - 1] = temp;
+      setAdvanceConfiguration(duplicateArr);
+    }
+  };
+
+  return (
+    <div>
+      <Hero
+        subtitle='Visualisation for understanding CSS Grid'
+        title='CSS Grid'
+      />
+      <div className='grid min-h-[36rem] w-full grid-cols-1 gap-5 rounded-md text-black md:grid-cols-7 lg:grid-cols-8'>
+        {/* LEFT */}
+        <div className='button-shadow flex flex-col gap-5 bg-stone-200 p-5 md:col-span-3'>
+          <RangeInput
+            aria-label='Count'
+            defaultValue={configuration.itemCount}
+            fullWidth
+            onChange={(e) =>
+              handleConfigurationChange({ itemCount: Number(e.target.value) })
+            }
+            range={[1, 15]}
+            title='Count'
+            titleClassName='bg-[#ff91e7]'
+          />
+          <RangeInput
+            aria-label='Columns'
+            defaultValue={configuration.columns}
+            fullWidth
+            onChange={(e) =>
+              handleConfigurationChange({ columns: Number(e.target.value) })
+            }
+            range={[1, 12]}
+            title='Columns'
+            titleClassName='bg-[#22a094]'
+          />
+          <RangeInput
+            aria-label='Gap'
+            defaultValue={configuration.gap}
+            fullWidth
+            max={'5'}
+            min={'0'}
+            onChange={(e) =>
+              handleConfigurationChange({ gap: Number(e.target.value) })
+            }
+            title='Gap'
+            titleClassName='bg-[#91a8ed]'
+            values={[0, 1, 2, 3, 4, 5]}
+          />
+          {/* Advance */}
+          <div className='button-shadow collapse-arrow collapse rounded-none border-4 border-[#1E1E1E] bg-white'>
+            <input type='checkbox' />
+            <div className='collapse-title text-xl font-medium'>
+              Advance Settings
+            </div>
+            <div className='collapse-content flex flex-col gap-2'>
+              {/* Column Span */}
+              <div className='collapse'>
+                <input type='checkbox' />
+                <div className='collapse-title underline'>Column Span</div>
+                <div className='collapse-content'>
+                  <SelectInput
+                    items={items}
+                    name='Span'
+                    onChange={(e, item) => {
+                      const { value } = e.target;
+                      handleAdvanceConfigurationChange(
+                        {
+                          colSpan: Number.isNaN(Number(value))
+                            ? 'auto'
+                            : Number(value),
+                        },
+                        item,
+                      );
+                    }}
+                  />
+                </div>
+              </div>
+
+              {/* Column Start */}
+              <div className='collapse'>
+                <input type='checkbox' />
+                <div className='collapse-title underline'>Column Start</div>
+                <div className='collapse-content'>
+                  <SelectInput
+                    items={items}
+                    name='Start'
+                    onChange={(e, item) => {
+                      const { value } = e.target;
+                      handleAdvanceConfigurationChange(
+                        {
+                          colStart: Number.isNaN(Number(value))
+                            ? 'auto'
+                            : Number(value),
+                        },
+                        item,
+                      );
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* RIGHT */}
+        <div className='button-shadow bg-stone-200 p-5 md:col-span-4 lg:col-span-5'>
+          <div
+            className={classNames(
+              'grid',
+              getGridCols(configuration.columns),
+              getGridGap(configuration.gap),
+            )}
+          >
+            {items.map((item, idx) => {
+              const colSpan = advanceConfiguration[idx]?.colSpan;
+              const colStart = advanceConfiguration[idx]?.colStart;
+              return (
+                <div
+                  className={classNames(
+                    'flex-center h-32 animate-fade bg-fuchsia-700 py-5 text-2xl font-semibold transition animate-once hover:bg-fuchsia-800',
+                    getColSpan(colSpan),
+                    getColStart(colStart),
+                  )}
+                  key={item}
+                >
+                  {item}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GridFlex;
