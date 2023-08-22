85344e235f43549c7f269eaa9e0015d9ea0aae31
Ahsan Khan <m.ahsankhan26@hotmail.com>
2023-08-22 10:11:29 +0500

    Fixes (#9)
    * fix: sort
    
    * chore: add icon on footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,5 +1,10 @@
+import { FcLike } from 'react-icons/fc';
+
 const Footer: React.FC = () => (
-  <div className='mx-auto my-10 mb-5 text-xs'>Made with ♥</div>
+  <div className='mx-auto my-10 mb-5 flex items-center gap-1 text-xs'>
+    <span>Made with</span>
+    <FcLike />
+  </div>
 );
 
 export default Footer;
