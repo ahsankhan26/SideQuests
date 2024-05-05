b8f34a6f3d0fff8e2a6a2246053096123ead8889
Ahsan Khan <ahsan@geeksofkolachi.com>
2024-05-05 16:21:05 +0500

    refactor: upgrade to v14

--- next.config.js
+++ next.config.js
@@ -4,32 +4,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withBundleAnalyzer({
-  experimental: {
-    appDir: true,
-  },
   eslint: {
     dirs: ['.'],
   },
-  poweredByHeader: false,
   basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
   reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-        port: '',
-        pathname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-        port: '',
-        pathname: '**',
-      },
-    ],
-  },
 });
